c177e117133c7be9dc43e7cd9c1cadf357679df3
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2018-11-12 23:48:47 -0500

    ajustements-layout

--- routes/calculator.js
+++ routes/calculator.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var finance = require('@riouxjean/test');
 
+function toPercent(num) {
+  var percent_c = num * 100;
+  var percent = Math.round(percent_c * 100) / 100
+  var string = percent.toString() + "%";
+  return string;
+}
+
 
 
 /* GET users listing. */
@@ -10,14 +17,7 @@ router.post('/', function(req, res, next) {
     var position = await finance(req.body.ticker);
     await res.render('result', {
       vti: position.vti,
-      ticker: req.body.ticker/*,
-      x7: position.x7,
-      x6: position.x6,
-      x5: position.x5,
-      x4: position.x4,
-      x3: position.x3,
-      x2: position.x2,
-      x1: position.x1*/,
+      ticker: req.body.ticker,
       eps: position.eps,
       shareprice: position.shareprice,
       pe: position.pe,
@@ -25,9 +25,16 @@ router.post('/', function(req, res, next) {
       ps: position.ps,
       pc: position.pc,
       de: position.de,
-      eg: position.eg,
-      pr: position.payout,
-      y: position.yield
+      eg: toPercent(position.eg),
+      pr: toPercent(position.payout),
+      y: toPercent(position.yield)/*,
+      x7: position.x7,
+      x6: position.x6,
+      x5: position.x5,
+      x4: position.x4,
+      x3: position.x3,
+      x2: position.x2,
+      x1: position.x1*/
      });
   }
   call();
